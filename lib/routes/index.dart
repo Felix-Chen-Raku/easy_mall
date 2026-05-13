43044a716874fca31d4c9785f6c29d912e98dca8
Felix Chen <felixchen186@gmail.com>
2026-05-13 12:46:51 +0800

    搭建基础路由

---
+++ lib/routes/index.dart
@@ -0,0 +1,20 @@
+// 管理路由
+import 'package:easy_mall/pages/Login/index.dart';
+import 'package:easy_mall/pages/Main/index.dart';
+import 'package:flutter/material.dart';
+
+// 返回App根级组件
+Widget getRootWidget (){
+  return MaterialApp(
+    // 命名路由
+    initialRoute: '/',
+    routes: getRootRoutes(),
+  );
+}
+// 返回该App的路由配置
+Map<String, Widget Function(BuildContext)> getRootRoutes (){
+  return {
+    '/':(context) => MainPage(), // 主页路由
+    '/login':(context) => LoginPage() // 登录路由
+  };
+}
