af1b888fb26c771ea8a1c4e75b9dae4895ed80b9
Felix Chen <felixchen186@gmail.com>
2026-05-25 23:31:18 +0800

    完成登录页及表单搭建

--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ToastUtils {
+  // 阀门控制
+  static bool shoeLoading = false;
   static void showToast (BuildContext context, String msg) {
+    if (ToastUtils.shoeLoading) {
+      return;
+    }
+    ToastUtils.shoeLoading = true;
+    Future.delayed(Duration(seconds: 3),(){
+      ToastUtils.shoeLoading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      width: 120,
+      width: 180,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(40)
       ),
       behavior: SnackBarBehavior.floating, // 悬浮状态
-      duration: Duration(seconds: 2),
+      duration: Duration(seconds: 3),
       content: Text(msg ?? '加载中',textAlign: TextAlign.center,)));
   }
 }
